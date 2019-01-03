5d1a121f5a3bdc273ad67a77a6dfc645107f705a
Deepu K Sasidharan <d4udts@gmail.com>
2019-01-03 13:53:18 +0100

    LOVE-430 add dependsON for vpc

--- gcp/microservice-ecommerce/terraform/vpc/main.tf
+++ gcp/microservice-ecommerce/terraform/vpc/main.tf
@@ -7,6 +7,7 @@ resource "google_compute_network" "vpc" {
 # Create Subnet
 
 resource "google_compute_subnetwork" "subnet" {
+  depends_on    = ["google_compute_network.vpc"]
   name          = "${var.name}-subnet"
   ip_cidr_range = "${var.subnet_cidr}"
   network       = "${var.name}-vpc"
@@ -16,6 +17,7 @@ resource "google_compute_subnetwork" "subnet" {
 # VPC firewall configuration
 # Create a firewall rule that allows internal communication across all protocols:
 resource "google_compute_firewall" "firewalli-int" {
+  depends_on    = ["google_compute_subnetwork.subnet"]
   name    = "${var.name}-firewall-int"
   network = "${var.name}-vpc"
 
@@ -36,6 +38,7 @@ resource "google_compute_firewall" "firewalli-int" {
 
 # Create a firewall rule that allows external SSH, ICMP, and HTTPS:
 resource "google_compute_firewall" "firewalli-ext" {
+  depends_on    = ["google_compute_subnetwork.subnet"]
   name    = "${var.name}-firewall-ext"
   network = "${var.name}-vpc"
 
