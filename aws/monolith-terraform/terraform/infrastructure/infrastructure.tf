b83db1d218847a11364d028f39f509535fc1b5fd
deepu105 <d4udts@gmail.com>
2019-02-06 18:18:27 +0100

    LOVE-503 provision and use  a unique log group

--- aws/monolith-terraform/terraform/infrastructure/infrastructure.tf
+++ aws/monolith-terraform/terraform/infrastructure/infrastructure.tf
@@ -187,3 +187,7 @@ resource "aws_iam_role_policy_attachment" "ecs-task-execution-role-attachment" {
   role       = "${aws_iam_role.ecs-task-execution-role.name}"
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
+
+resource "aws_cloudwatch_log_group" "cloudwatch_log" {
+  name = "/ecs/${var.app_name}"
+}
