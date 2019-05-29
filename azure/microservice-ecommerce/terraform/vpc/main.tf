655287b9283dcfc6634ce753ee3383752558cd01
Jeffrey Johnson <sffortytwo@gmail.com>
2019-05-29 13:41:03 +0200

    Addressed Deepu's questions

--- azure/microservice-ecommerce/terraform/vpc/main.tf
+++ azure/microservice-ecommerce/terraform/vpc/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_virtual_network" "vnet" {
-  name                = "jeff-vnet"
+  name                = "${var.name}-vnet"
   location            = "${var.location}"
   resource_group_name = "${var.resource_group_name}"
   address_space       = ["10.1.0.0/16"]
@@ -7,20 +7,20 @@ resource "azurerm_virtual_network" "vnet" {
 
 resource "azurerm_subnet" "subnet" {
   depends_on           = ["azurerm_virtual_network.vnet"]
-  name                 = "internal"
+  name                 = "${var.name}-internal"
   resource_group_name  = "${var.resource_group_name}"
   address_prefix       = "10.1.2.0/24"
   virtual_network_name = "${azurerm_virtual_network.vnet.name}"
 }
 
 resource "azurerm_network_security_group" "security" {
-  name                = "jjohnson-nsg"
+  name                = "${var.name}-nsg"
   location            = "${var.location}"
   resource_group_name = "${var.resource_group_name}"
 }
 
 resource "azurerm_network_security_rule" "ssh_https" {
-  name                        = "ssh_https"
+  name                        = "${var.name}-ssh_https"
   priority                    = 100
   direction                   = "Inbound"
   access                      = "Allow"
