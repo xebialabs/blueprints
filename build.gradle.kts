b88f2b26d87ec44f6a24c2d4dc9940ff53ed9689
Vedran Pugar <72854396+vpugar-digital@users.noreply.github.com>
2022-03-16 04:25:07 +0100

    ENG-8955 add build for 10.2.x (#130)

---
+++ build.gradle.kts
@@ -0,0 +1,222 @@
+import com.github.gradle.node.yarn.task.YarnTask
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.io.ByteArrayOutputStream
+
+buildscript {
+    repositories {
+        mavenLocal()
+        gradlePluginPortal()
+        arrayOf("releases", "public").forEach { r ->
+            maven {
+                url = uri("${project.property("nexusBaseUrl")}/repositories/${r}")
+                credentials {
+                    username = project.property("nexusUserName").toString()
+                    password = project.property("nexusPassword").toString()
+                }
+            }
+        }
+    }
+
+    dependencies {
+        classpath("com.xebialabs.gradle.plugins:gradle-commit:${properties["gradleCommitPluginVersion"]}")
+        classpath("com.xebialabs.gradle.plugins:gradle-xl-defaults-plugin:${properties["xlDefaultsPluginVersion"]}")
+    }
+}
+
+plugins {
+    kotlin("jvm") version "1.4.20"
+
+    id("com.github.node-gradle.node") version "3.1.0"
+    id("idea")
+    id("nebula.release") version (properties["nebulaReleasePluginVersion"] as String)
+    id("maven-publish")
+}
+
+apply(plugin = "ai.digital.gradle-commit")
+
+group = "ai.digital.xlclient.blueprints"
+project.defaultTasks = listOf("build")
+
+val releasedVersion = System.getenv()["RELEASE_EXPLICIT"] ?: if (project.version.toString().contains("SNAPSHOT")) {
+    project.version.toString()
+} else {
+    "10.2.0-${LocalDateTime.now().format(DateTimeFormatter.ofPattern("Mdd.Hmm"))}"
+}
+project.extra.set("releasedVersion", releasedVersion)
+
+repositories {
+    mavenLocal()
+    gradlePluginPortal()
+    maven {
+        url = uri("https://plugins.gradle.org/m2/")
+    }
+}
+
+idea {
+    module {
+        setDownloadJavadoc(true)
+        setDownloadSources(true)
+    }
+}
+
+dependencies {
+    implementation(gradleApi())
+    implementation(gradleKotlinDsl())
+
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
+}
+
+tasks {
+    register("dumpVersion") {
+        doLast {
+            project.logger.lifecycle("Dumping version $releasedVersion")
+            file(buildDir).mkdirs()
+            file("$buildDir/version.dump").writeText("version=${releasedVersion}")
+        }
+    }
+
+    named<YarnTask>("yarn_install") {
+        args.set(listOf("--mutex", "network"))
+        workingDir.set(file("${rootDir}/documentation"))
+    }
+
+    register<YarnTask>("yarnRunStart") {
+        dependsOn(named("yarn_install"))
+        args.set(listOf("run", "start"))
+        workingDir.set(file("${rootDir}/documentation"))
+    }
+
+    register<YarnTask>("yarnRunBuild") {
+        dependsOn(named("yarn_install"))
+        args.set(listOf("run", "build"))
+        workingDir.set(file("${rootDir}/documentation"))
+    }
+
+    register<Delete>("docCleanUp") {
+        delete(file("${rootDir}/docs"))
+        delete(file("${rootDir}/documentation/build"))
+        delete(file("${rootDir}/documentation/.docusaurus"))
+        delete(file("${rootDir}/documentation/node_modules"))
+    }
+
+    register<Copy>("docBuild") {
+        dependsOn(named("yarnRunBuild"), named("docCleanUp"))
+        from(file("${rootDir}/documentation/build"))
+        into(file("${rootDir}/docs"))
+    }
+
+    register<GenerateDocumentation>("updateDocs") {
+        dependsOn(named("docBuild"))
+    }
+
+    register<Copy>("blueprintsCopy") {
+        from("./") {
+            include("aws/**/*")
+            include("azure/**/*")
+            include("blueprint-skeleton/**/*")
+            include("devsecops/**/*")
+            include("docker/**/*")
+            include("gcp/**/*")
+            include("kubernetes/**/*")
+            include("xl-devops-platform/**/*")
+            include("*.txt")
+            include("README.md")
+            include("*.yaml")
+            include("*.json")
+            include("*.py")
+        }
+        into(layout.buildDirectory.dir("blueprints"))
+    }
+
+    register<Exec>("generateIndexJson") {
+        dependsOn("blueprintsCopy")
+        workingDir(layout.buildDirectory.dir("blueprints"))
+        commandLine("python", "./generate_index.py")
+
+        standardOutput = ByteArrayOutputStream()
+        errorOutput = ByteArrayOutputStream()
+
+        doLast {
+            logger.lifecycle(standardOutput.toString())
+            logger.error(errorOutput.toString())
+            logger.lifecycle("Generated index.json")
+        }
+    }
+
+    register<Zip>("blueprintsArchives") {
+        dependsOn("generateIndexJson")
+        from(layout.buildDirectory.dir("blueprints")) {
+            include("**/*.*")
+            exclude("*.py")
+            archiveBaseName.set("blueprints")
+            archiveVersion.set(releasedVersion)
+            archiveExtension
+        }
+    }
+
+    register<NebulaRelease>("nebulaRelease") {
+        dependsOn(named("buildOperators"), named("updateDocs"))
+    }
+
+    register<Exec>("syncBlueprintsArchives") {
+        dependsOn("blueprintsArchives")
+        val command =
+            "ssh xebialabs@nexus1.xebialabs.cyso.net rsync -razv --delete --chmod=Du=rwx,Dg=rx,Do=rx,Fu=rw,Fg=r,Fo=r " +
+                    "---exclude '*.py' ${layout.buildDirectory.dir("blueprints").get().asFile.absolutePath} " +
+                    "xldown@dist.xebialabs.com:/var/www/dist.xebialabs.com/public/blueprints/$releasedVersion"
+        commandLine(command.split(" "))
+    }
+
+    register("syncToDistServer") {
+        dependsOn("syncBlueprintsArchives")
+    }
+
+    named<Upload>("uploadArchives") {
+        dependsOn(named("dumpVersion"))
+        dependsOn(named("publish"))
+    }
+
+    register("buildOperators") {
+        dependsOn("blueprintsArchives")
+    }
+
+    register("checkDependencyVersions") {
+        // a placeholder to unify with release in jenkins-job
+    }
+}
+
+publishing {
+    publications {
+        register("blueprints-archive", MavenPublication::class) {
+            artifact(tasks["blueprintsArchives"]) {
+                artifactId = "blueprints"
+                version = releasedVersion
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            url = uri("${project.property("nexusBaseUrl")}/repositories/digitalai-public")
+            credentials {
+                username = project.property("nexusUserName").toString()
+                password = project.property("nexusPassword").toString()
+            }
+        }
+    }
+}
+
+node {
+    version.set("16.13.2")
+    yarnVersion.set("1.22.17")
+    download.set(true)
+}
